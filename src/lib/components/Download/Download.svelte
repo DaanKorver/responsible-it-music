af204d06a6ff52f92e683b1f01e8f92a111c2a97
DaanKorver <daank2000@hotmail.nl>
2023-03-23 11:32:56 +0100

    Download button

---
+++ src/lib/components/Download/Download.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { record } from '$lib/utils/record';
+	import Modal from './Modal.svelte';
+
+	let open = false;
+
+	function onClick() {
+		open = true;
+	}
+
+	$: {
+		if (open) {
+			record();
+		}
+	}
+</script>
+
+<button on:click={onClick}>Download</button>
+
+<Modal isOpen={open} />
+
+<style>
+	button {
+		position: fixed;
+		padding: 1rem 2rem;
+		top: 2rem;
+		left: 2rem;
+	}
+</style>
