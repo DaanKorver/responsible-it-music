f124bfd8554fff26dd219ce6ed6b53c352e0364d
DaanKorver <daank2000@hotmail.nl>
2023-03-23 12:46:29 +0100

    Removed feedback temp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	import FeedbackButton from '$lib/components/Feedback/FeedbackButton.svelte';
+	import Modal from '$lib/components/Download/Modal.svelte';
 	import Sampler from '$lib/components/Sampler/Sampler.svelte';
+	import Download from '$lib/components/Download/Download.svelte';
 </script>
 
-<FeedbackButton />
+<!-- <FeedbackButton /> -->
 <div>
 	<Sampler />
 </div>
+<Download />
 
 <style>
 	div {
